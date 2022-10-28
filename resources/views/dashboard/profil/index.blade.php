6a1158837a5b2878982b5fa8cd5e8fd7fecc9ca9
HIilLm <97495105+HIilLm@users.noreply.github.com>
2022-10-28 08:38:24 +0700

    fix create siswa

--- resources/views/dashboard/profil/index.blade.php
+++ resources/views/dashboard/profil/index.blade.php
@@ -26,30 +26,5 @@
     </style>
 @endsection
 @section('content')
-@if($siswa == null)
-<a  class="btn btn-primary" href="/dashboard/siswa/create">Make Siswa</a>
-@endif
-@isset($siswa)
-        <div class="card mb-4">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        @if ($siswa->first() != null)
-                            <div class="gambar" style="background-image: url('{{ asset('storage/' . $siswa->profil) }}')"></div>
-                            {{-- <img class="img-profile rounded-circle"
-            src="{{ asset('storage/' . $siswa->profil) }}"> --}}
-                        @else
-                            <div class="gambar2"></div>
-                        @endif
-
-                        <h1>{{ $siswa->name }}</h1>
-                        <p>{{ $siswa->about }}</p>
-                        <a href="{{ route('dashboard.edit', ['dashboard' => $siswa->id]) }}" class="btn btn-primary">Edit</a>
-
-                    </div>
-                </div>
-
-            </div>
-        @endisset
-
-    @endsection
+Halo Admin
+@endsection
--- resources/views/dashboard/siswa/create.blade.php
+++ resources/views/dashboard/siswa/create.blade.php
@@ -83,17 +83,17 @@
                             <div class="col">
                                 <div class="mb-3">
                                     <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
-                                    <select class="form-select" aria-label="Default select example">
-                                        <option selected>Open this select menu</option>
-                                        <option value="1">One</option>
-                                        <option value="2">Two</option>
-                                        <option value="3">Three</option>
-                                      </select>
-                                    @error('jenis_kelamin')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
+                                    <select name="jenis_kelamin" class="form-control @error('jenis_kelamin') is-invalid @enderror" id="jenis_kelamin"
+                                    aria-label="Default select example">
+                                    <option value="">Open this select menu</option>
+                                    <option value="Laki-Laki">Laki-Laki</option>
+                                    <option value="Perempuan">Perempuan</option>
+                                </select>
+                                @error('jenis_kelamin')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                                 </div>
                             </div>
                         </div>
